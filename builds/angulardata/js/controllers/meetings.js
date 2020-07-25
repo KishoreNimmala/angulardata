20a6c56df9f6cd8604a47bca4825cb7aa99f518e
Kishore Nimmala <nkishore779@gmail.com>
2020-07-26 01:07:07 +0530

    AngularData adding & deleting meetings

--- builds/angulardata/js/controllers/meetings.js
+++ builds/angulardata/js/controllers/meetings.js
@@ -1,3 +1,34 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
-  $scope.message = "Success!!!";
-}]);
+myApp.controller('MeetingsController', 
+	['$scope','$rootScope','$firebaseAuth','$firebaseArray', 
+	function($scope,$rootScope,$firebaseAuth,$firebaseArray) {
+  		var ref = firebase.database().ref();
+  		var auth = $firebaseAuth();
+  		auth.$onAuthStateChanged(function(authUser) {
+		    if(authUser) {
+		      var meetingRef=ref.child('users').child(authUser.uid).child('meetings');
+		      var meetingsInfo=$firebaseArray(meetingRef);
+		      $scope.meetings=meetingsInfo;
+
+		      meetingsInfo.$loaded().then(function(data){
+		      	$rootScope.howManyMeetings=meetingsInfo.length;
+		      });// make sure meeting data is loaded
+
+		      meetingsInfo.$watch(function(data){
+		      	$rootScope.howManyMeetings=meetingsInfo.length;
+		      });//Update meetings chages
+
+		      $scope.addMeeting=function(){
+		      	meetingsInfo.$add({
+		      		name:$scope.meetingname,
+		      		data:firebase.database.ServerValue.TIMESTAMP
+		      	}).then(function(){
+		      		$scope.meetingname='';
+		      	});//promise
+		      }//addMeeting
+
+		      $scope.deleteMeeting=function(key){
+		      	meetingsInfo.$remove(key);
+		      }//deletemeeting
+		    } //authUser
+	  	});//onAuthStateChanged
+}]); //myApp.controller
